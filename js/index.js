1c419a12b212068383b94254edee15c248bc6927
Mels <melissagaleanomesa@gmail.com>
2022-08-13 16:16:19 -0300

    file adjustment

--- js/index.js
+++ js/index.js
@@ -1,14 +1,48 @@
-document.addEventListener("DOMContentLoaded", function(){
-    document.getElementById("autos").addEventListener("click", function() {
-        localStorage.setItem("catID", 101);
-        window.location = "products.html"
-    });
-    document.getElementById("juguetes").addEventListener("click", function() {
-        localStorage.setItem("catID", 102);
-        window.location = "products.html"
-    });
-    document.getElementById("muebles").addEventListener("click", function() {
-        localStorage.setItem("catID", 103);
-        window.location = "products.html"
-    });
+
+const button = document.querySelector('.btn-login');
+const email = document.querySelector('.email');
+const pass = document.querySelector('.pass');
+const invalid = document.querySelectorAll('.invalid-feedback');
+
+button.addEventListener('click',(e) =>{
+    e.preventDefault();
+    
+    if([email.value, pass.value].includes("") ){
+        email.classList.add('form-control')
+        pass.classList.add('form-control')
+
+        for (let i =0; i < 3; i++){
+            invalid[i].style.display = 'block';
+        }
+        return;
+    }
+    window.location.href = "https://melissagaleano.github.io/home.html";
 });
+
+email.addEventListener('keydown', () => {
+    if(email.value !== '') {
+        email.classList.remove('form-control')
+        for (let i =0; i < 3; i++){
+            invalid[0].style.display = 'none';
+        }
+    } else if(email.value == '') {
+        email.classList.add('form-control')
+        for (let i =0; i < 3; i++){
+            invalid[0].style.display = 'block';
+    }}
+})
+
+
+pass.addEventListener('keydown', () => {
+    if(pass.value !== '') {
+        pass.classList.remove('form-control')
+        for (let i =0; i < 3; i++){
+            invalid[1].style.display = 'none';
+        }
+    } else if(pass.value == ''){
+        pass.classList.add('form-control')
+        for (let i =0; i < 3; i++){
+            invalid[1].style.display = 'block';
+        }
+    }
+})
